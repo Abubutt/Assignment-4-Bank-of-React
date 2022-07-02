7011397cb684188a68053457987f681fbab49daa
azedede <aidri5151@yahoo.com>
2022-07-02 09:34:53 -0400

    i nclude the addCredit and addDebit && credit and debit pages

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Assignment-4-Bank-of-React
 
-run:
-npm i
-npm i react-router-dom@5.3.0 react-router@5.2.1
-npm start
+# run:
+
+npm i react-router-dom@5.3.0 react-router@5.2.1 && npm start
--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,24 +1,36 @@
+import React from 'react'
 import { Link } from "react-router-dom";
 
-const Credits = (props) => {
-  let creditsView = () => {
-    const { credits } = props;
-    return credits.map((credit) => {
+const Credits = ({
+    addCredit, 
+    balance, 
+    credits
+}) => {
+
+  const creditsView = () => {
+    return credits?.map((credit, index) => {
       let date = credit.date.slice(0, 10);
       return (
-        <li key={credit.id}>
-          {credit.amount} {credit.description} {date}
+        <li key={index}>
+          {credit.amount} 
+          {credit.description}
+          {date}
         </li>
       );
     });
   };
+
+
   return (
     <div>
       <h1>Credit</h1>
+      <h4>Balance: {balance}</h4>
       {creditsView()}
-      <form onSubmit={props.addCredit}>
-        <input type="text" name="description" />
-        <input type="number" name="amount" />
+      <form onSubmit={addCredit}>
+        <label id="description"> Description: </label>
+        <input type="text" name="description"/>
+        <label id="amount"> Amount: </label>
+        <input type="number" name="amount"/>
         <button type="submit">Add Credits</button>
       </form>
       <Link to="/">Home</Link>
